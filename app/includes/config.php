1e4595bea26f026ada62a4d1707163fc5e30dc19
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-11-04 17:02:25 +0800

    Migrate project from XAMPP to Docker

---
+++ app/includes/config.php
@@ -0,0 +1,6 @@
+<?php
+  define('DB_HOST', 'db');          // Set database host
+  define('DB_USER', 'user');        // Set database user (update to 'user')
+  define('DB_PASS', 'password');     // Set database password (update to 'password')
+  define('DB_NAME', 'inventory_system'); // Set database name
+?>
