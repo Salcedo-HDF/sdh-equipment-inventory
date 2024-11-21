a541d560b973c47955b01cf719c3af038981d230
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-11-21 16:23:42 +0800

    Update

--- app/layouts/user_menu.php
+++ app/layouts/user_menu.php
@@ -6,24 +6,15 @@
     </a>
   </li>
   <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-th-list"></i>
-       <span>Sales</span>
-      </a>
-      <ul class="nav submenu">
-         <li><a href="sales.php">Manage Sales</a> </li>
-         <li><a href="add_sale.php">Add Sale</a> </li>
-     </ul>
+    <a href="sales.php">
+        <i class="glyphicon glyphicon-th-list"></i>
+        <span>Check In Item List</span>
+    </a>
   </li>
   <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-signal"></i>
-       <span>Sales Report</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
-      </ul>
+    <a href="sales.php">
+        <i class="glyphicon glyphicon-th-list"></i>
+        <span>Check Out Item List</span>
+    </a>
   </li>
 </ul>
