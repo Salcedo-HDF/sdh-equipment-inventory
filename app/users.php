507a24f6b062a9a58330fb62d01d6bf14e30c565
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-11-27 11:00:41 +0800

    Fix Item Id

--- app/users.php
+++ app/users.php
@@ -38,30 +38,30 @@
           </tr>
         </thead>
         <tbody>
-        <?php foreach($all_users as $a_user): ?>
+        <?php foreach($all_users as $index => $a_user): ?>
           <tr>
-           <td class="text-center"><?php echo count_id();?></td>
-           <td><?php echo remove_junk(ucwords($a_user['name']))?></td>
-           <td><?php echo remove_junk(ucwords($a_user['username']))?></td>
-           <td class="text-center"><?php echo remove_junk(ucwords($a_user['group_name']))?></td>
-           <td class="text-center">
-           <?php if($a_user['status'] === '1'): ?>
-            <span class="label label-success"><?php echo "Active"; ?></span>
-          <?php else: ?>
-            <span class="label label-danger"><?php echo "Deactive"; ?></span>
-          <?php endif;?>
-           </td>
-           <td><?php echo read_date($a_user['last_login'])?></td>
-           <td class="text-center">
-             <div class="btn-group">
+            <td class="text-center"><?php echo $index + 1; ?></td>
+            <td><?php echo remove_junk(ucwords($a_user['name']))?></td>
+            <td><?php echo remove_junk(ucwords($a_user['username']))?></td>
+            <td class="text-center"><?php echo remove_junk(ucwords($a_user['group_name']))?></td>
+            <td class="text-center">
+            <?php if($a_user['status'] === '1'): ?>
+              <span class="label label-success"><?php echo "Active"; ?></span>
+            <?php else: ?>
+              <span class="label label-danger"><?php echo "Deactive"; ?></span>
+            <?php endif;?>
+            </td>
+            <td><?php echo read_date($a_user['last_login'])?></td>
+            <td class="text-center">
+              <div class="btn-group">
                 <a href="edit_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit">
                   <i class="glyphicon glyphicon-pencil"></i>
-               </a>
+                </a>
                 <a href="delete_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-danger" data-toggle="tooltip" title="Remove">
                   <i class="glyphicon glyphicon-remove"></i>
                 </a>
-                </div>
-           </td>
+              </div>
+            </td>
           </tr>
         <?php endforeach;?>
        </tbody>
