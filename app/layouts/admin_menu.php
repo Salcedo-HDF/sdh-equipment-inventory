ae4433f92cbe720df96b6356badfba354964dfda
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-11-29 12:26:37 +0800

    Add requests

--- app/layouts/admin_menu.php
+++ app/layouts/admin_menu.php
@@ -55,6 +55,7 @@
     <ul class="nav submenu">
       <li><a href="log_in_out.php">Log In/Log out</a> </li>
       <li><a href="check_in_out.php">Check In/Check Out</a> </li>
+      <li><a href="requests.php">Requests</a> </li>
    </ul>
   </li>
 </ul>
