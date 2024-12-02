dbba02a8e42c60ea53d1e7b831476964946b905d
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-12-02 14:31:20 +0800

    Add log for log out

--- app/logout.php
+++ app/logout.php
@@ -1,4 +1,19 @@
 <?php
-  require_once('includes/load.php');
-  if(!$session->logout()) {redirect("index.php");}
+require_once('includes/load.php');
+
+$user_id = (int)$session->user_id;
+
+$date = date('Y-m-d H:i:s'); 
+$action = "Log out";
+
+if($user_id) {
+  $query = "INSERT INTO inout_logs (user_id, date, action) VALUES ('$user_id', '$date', '$action')";
+  $db->query($query); 
+}
+
+if(!$session->logout()) {
+  redirect("index.php");
+} else {
+  redirect("index.php");
+}
 ?>
