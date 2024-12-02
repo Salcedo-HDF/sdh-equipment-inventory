f8684054136aebd335943676f384c1777dc648bf
R-jhel B. Tandugon <jherietandugon@gmail.com>
2024-12-02 15:58:51 +0800

    Link Bootstrap

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -10,8 +10,8 @@
            echo ucfirst($user['name']);
             else echo "Inventory Management System";?>
     </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+    <link rel="stylesheet" href="libs/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="libs/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
   </head>
   <body>
